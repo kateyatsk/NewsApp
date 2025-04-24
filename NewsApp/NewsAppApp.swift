35690de5b7d15cad2f13b1fb6cfdaa9027b795ea
kateyatsk <klaettoe2023@gmail.com>
2025-04-24 14:19:31 +0300

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Екатерина Яцкевич on 24.04.25.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
